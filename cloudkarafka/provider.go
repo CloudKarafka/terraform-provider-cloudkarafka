a0b49d724add5159993f1b1dd29fab9d04443101
Tobias Brodén <tobias@84codes.com>
2020-02-06 13:57:32 +0100

    Upgrade to new terraform-plugin-sdk

--- cloudkarafka/provider.go
+++ cloudkarafka/provider.go
@@ -5,8 +5,8 @@ import (
 	"log"
 
 	"github.com/84codes/go-api/api"
-	"github.com/hashicorp/terraform/helper/schema"
-	"github.com/hashicorp/terraform/terraform"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
 
 var version string
--- cloudkarafka/resource_instance.go
+++ cloudkarafka/resource_instance.go
@@ -2,7 +2,7 @@ package cloudkarafka
 
 import (
 	"github.com/84codes/go-api/api"
-	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
 
 func resourceInstance() *schema.Resource {
